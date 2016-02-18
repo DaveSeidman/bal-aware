fd2cbae00b3442c13e89983433263ee3303e2375
Dave Seidman <dave.seidman@b-reel.com>
2016-02-18 11:44:11 -0500

    moving config info

--- index.js
+++ index.js
@@ -3,31 +3,11 @@ var Banking = require('banking');
 var parseString = require('xml2js').parseString;
 
 var checking = Banking({
-    fid: 5959
-    , fidOrg: 'HAN'
-    , url: 'https://eftx.bankofamerica.com/eftxweb/access.ofx'
-    , bankId: '021000322' /* If bank account use your bank routing number otherwise set to null */
-    , user: 'daveseidman'
-    , password: 'baFingernail1'
-    , accId: '483005421227' /* Account Number */
-    , accType: 'CHECKING' /* CHECKING || SAVINGS || MONEYMRKT || CREDITCARD */
-    , ofxVer: 103 /* default 102 */
-    , app: 'QWIN' /* default  'QWIN' */
-    , appVer: '1700' /* default 1700 */
+
 });
 
 var credit = Banking({
-    fid: 5959
-    , fidOrg: 'HAN'
-    , url: 'https://eftx.bankofamerica.com/eftxweb/access.ofx'
-    , bankId: '021000322' /* If bank account use your bank routing number otherwise set to null */
-    , user: 'daveseidman'
-    , password: 'baFingernail1'
-    , accId: '4400664846144377' /* Account Number */
-    , accType: 'CREDITCARD' /* CHECKING || SAVINGS || MONEYMRKT || CREDITCARD */
-    , ofxVer: 103 /* default 102 */
-    , app: 'QWIN' /* default  'QWIN' */
-    , appVer: '1700' /* default 1700 */
+    
 });
 
 
