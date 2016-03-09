ca897a43de6623f8c542673baa131d8958e02430
Dave Seidman <dave.seidman@b-reel.com>
2016-03-09 17:37:28 -0500

    general updates

--- index.js
+++ index.js
@@ -26,9 +26,44 @@ function getBalanceForAccount(i) {
 
 function getBalanceForDate(account, date, callback) {
 
-    var dateFormatted = formatDate(date);
 
-    account.getStatement({start:dateFormatted, end:dateFormatted}, function(err, res) {
+    //console.log(date.getDay());
+
+    //var dateFormatted = formatDate(date);
+    var monday = new Date();
+    monday.setDate(date.getDate() - date.getDay() + 1);
+
+    account.getStatement({start:formatDate(monday), end:formatDate(date)}, function(err, res) {
+
+        //console.log(res);
+        parseString(res.xml, function (err, result) {
+
+            var ofx = result.OFX;
+
+            searchJSON(ofx, 'BANKTRANLIST2');
+
+            function searchJSON(obj, search) {
+                for (var k in obj) {
+                    if (typeof obj[k] == "object" && obj[k] !== null) {
+                        if(k == search) {
+                            //return (obj[k]);
+                            console.log(obj[k]);
+                            break;
+                        }
+                        else searchJSON(obj[k], search);
+                    }
+                }
+            }
+            //var transactions = getValues(ofx, 'BANKTRANLIST');
+            //console.log(Object.keys(ofx));
+            // for(var key in transactions) {
+            //     console.log(transactions[key]);
+            // }
+        });
+        //return callback("test");
+
+    });
+    /*account.getStatement({start:dateFormatted, end:dateFormatted}, function(err, res) {
 
         if(err) {
             console.log("there was a banking error", err);
@@ -43,17 +78,49 @@ function getBalanceForDate(account, date, callback) {
                 else return callback("error");
             });
 
-            /*var date = new Date();
-            dateOffset++;
-            date.setDate(today.getDate() - dateOffset);
-            if(date.getDay() > 0) getBalanceForDate(date);*/
+            // var date = new Date();
+            // dateOffset++;
+            // date.setDate(today.getDate() - dateOffset);
+            // if(date.getDay() > 0) getBalanceForDate(date);
 
         }
-    });
+    });*/
 }
 
+function getValues(obj, key) {
+    var objects = [];
+    for (var i in obj) {
+        if (!obj.hasOwnProperty(i)) continue;
+        if (typeof obj[i] == 'object') {
+            objects = objects.concat(getValues(obj[i], key));
+        } else if (i == key) {
+            objects.push(obj[i]);
+        }
+    }
+    return objects;
+}
 
-function getObjects(obj, key) {
+function getObjects(obj, key, val) {
+    var objects = [];
+    for (var i in obj) {
+        if (!obj.hasOwnProperty(i)) continue;
+        if (typeof obj[i] == 'object') {
+            objects = objects.concat(getObjects(obj[i], key, val));
+        } else
+        //if key matches and value matches or if key matches and value is not passed (eliminating the case where key matches but passed value does not)
+        if (i == key && obj[i] == val || i == key && val == '') { //
+            objects.push(obj);
+        } else if (obj[i] == val && key == ''){
+            //only add if the object is not already in the array
+            if (objects.lastIndexOf(obj) == -1){
+                objects.push(obj);
+            }
+        }
+    }
+    return objects;
+}
+
+/*function getObjects(obj, key) {
     var objects = [];
     for (var i in obj) {
         if(i == key) objects.push(obj);
@@ -63,7 +130,7 @@ function getObjects(obj, key) {
         }
     }
     return objects;
-}
+}*/
 
 function formatDate(date) {
 
