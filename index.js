3539b0452eb119426c786b70ebe43a67a88a0941
Dave Seidman <dave.seidman@b-reel.com>
2016-03-09 10:42:15 -0500

    Now loops through all accounts

--- index.js
+++ index.js
@@ -2,28 +2,52 @@ var Banking = require('banking');
 var accounts = require('./private/accounts.json');
 var parseString = require('xml2js').parseString;
 
-var today = new Date();
-var todayFormatted =
-today.getFullYear() + '' +
-(((today.getMonth() + 1) < 10) ? ('0' + (today.getMonth() + 1)) : (today.getMonth() + 1)) + '' +
-((today.getDate() < 10) ? ('0' + today.getDate()) : today.getDate() );
+
+var dateOffset = 0;
 
 
-for(var i = 0; i < accounts.data.length; i++) {
-    var account = accounts.data[i];
-    var bankAccount = Banking(account);
-    bankAccount.getStatement({start:todayFormatted, end:todayFormatted}, function(err, res) {
+getBalanceForAccount(0);
+
+
+function getBalanceForAccount(i) {
+
+    if(i < accounts.data.length) {
+        var account = Banking(accounts.data[i]);
+        var today = new Date();
+        getBalanceForDate(account, today, function(data) {
+            console.log("done:", data);
+            i++;
+            getBalanceForAccount(i);
+        });
+    }
+}
+
+
+
+function getBalanceForDate(account, date, callback) {
+
+    var dateFormatted = formatDate(date);
+
+    account.getStatement({start:dateFormatted, end:dateFormatted}, function(err, res) {
 
         if(err) {
             console.log("there was a banking error", err);
+            return callback();
         }
         else {
             parseString(res.xml, function (err, result) {
 
                 var ofx = result.OFX;
                 var bal = getObjects(ofx, 'LEDGERBAL');
-                console.log(bal[0].LEDGERBAL[0].BALAMT[0]);
-            })
+                if(bal.length) return callback(bal[0].LEDGERBAL[0].BALAMT[0]);
+                else return callback("error");
+            });
+
+            /*var date = new Date();
+            dateOffset++;
+            date.setDate(today.getDate() - dateOffset);
+            if(date.getDay() > 0) getBalanceForDate(date);*/
+
         }
     });
 }
@@ -39,4 +63,11 @@ function getObjects(obj, key) {
         }
     }
     return objects;
+}
+
+function formatDate(date) {
+
+    return date.getFullYear() + '' +
+    (((date.getMonth() + 1) < 10) ? ('0' + (date.getMonth() + 1)) : (date.getMonth() + 1)) + '' +
+    ((date.getDate() < 10) ? ('0' + date.getDate()) : date.getDate() );
 }
